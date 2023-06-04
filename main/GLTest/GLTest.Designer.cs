90ff9e367d8b83adf81c8bca64e97af63596bdc6
Marcus André <ddtank.marcus@gmail.com>
2023-06-04 01:27:21 -0300

    Backup Panel before be removed

--- main/GLTest/GLTest.Designer.cs
+++ main/GLTest/GLTest.Designer.cs
@@ -40,6 +40,7 @@ namespace GLTest
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -141,11 +142,23 @@ namespace GLTest
             this.button9.UseVisualStyleBackColor = true;
             this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
+            // button10
+            // 
+            this.button10.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button10.Location = new System.Drawing.Point(733, 721);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(95, 23);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Square Test";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
             // GLTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1262, 756);
+            this.Controls.Add(this.button10);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
@@ -173,5 +186,6 @@ namespace GLTest
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
     }
 }
---
+++ main/OrbisGL/Controls/Panel.cs
@@ -0,0 +1,109 @@
+﻿using OrbisGL.GL;
+using OrbisGL.GL2D;
+using SharpGLES;
+using SixLabors.ImageSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrbisGL.Controls
+{
+    public class Panel : Control
+    {
+        Texture2D Rectangle;
+        int FrameBuffer;
+        public Panel(string Name, Vector2 Position, Vector2 Size)
+        {
+            this.Name = Name;
+            this.Position = Position;
+            this.Size = Size;
+
+            int[] buffer = new int[1];
+            GLES20.GenFramebuffers(1, buffer);
+            FrameBuffer = buffer.Single();
+
+            Rectangle = new Texture2D();
+            Rectangle.Texture = Cache = new Texture(true);
+        }
+
+        public override Vector2 RenderPosition => Vector2.Zero;
+
+        Texture Cache;
+        bool Invalidated = false;
+
+        public override void Invalidate()
+        {
+            Invalidated = true;
+            base.Invalidate();
+        }
+        public override bool Focusable => false;
+
+        public override string Name { get; set; }
+
+        public override void Draw(long Tick)
+        {
+            int OriWidth = Coordinates2D.Width;
+            int OriHeight = Coordinates2D.Height;
+            int OriBuffer = GLES20.CurrentFrameBuffer;
+
+            if (Invalidated)
+            {
+                try
+                {
+
+                    GLES20.Viewport(0, 0, (int)Size.X, (int)Size.Y);
+                    Coordinates2D.SetSize((int)Size.X, (int)Size.Y);
+                    GLES20.BindFrameBuffer(GLES20.GL_FRAMEBUFFER, FrameBuffer);
+
+
+                    Rectangle.Width = (int)Size.X;
+                    Rectangle.Height = (int)Size.Y;
+                    Rectangle.RefreshVertex();
+
+                    Cache.Bind();
+
+                    GLES20.TexImage2D(GLES20.GL_TEXTURE_2D, 0, GLES20.GL_RGBA, (int)Size.X, (int)Size.Y, 0, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, IntPtr.Zero);
+
+                    GLES20.TexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_NEAREST);
+                    GLES20.TexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
+
+                    GLES20.FramebufferTexture2D(GLES20.GL_FRAMEBUFFER, GLES20.GL_COLOR_ATTACHMENT0, GLES20.GL_TEXTURE_2D, Cache.TextureID, 0);
+
+                    if (GLES20.CheckFramebufferStatus(GLES20.GL_FRAMEBUFFER) != GLES20.GL_FRAMEBUFFER_COMPLETE)
+                    {
+                        throw new Exception("Failed to Render to the texture");
+                    }
+
+                    base.Draw(Tick);
+                    GLES20.Flush();
+
+                    Invalidated = false;
+                }
+                finally
+                {
+                    GLES20.BindFrameBuffer(GLES20.GL_FRAMEBUFFER, OriBuffer);
+
+                    GLES20.Viewport(0, 0, OriWidth, OriHeight);
+                    Coordinates2D.SetSize(OriWidth, OriHeight);
+
+
+                    //Rectangle.RefreshVertex();
+                }
+            }
+
+            Rectangle.Position = new Vector3(Position, -1);
+            Rectangle.Draw(Tick);
+        }
+
+        public override void Dispose()
+        {
+            foreach (var Child in Childs)
+                Child.Dispose();
+
+            Rectangle.Dispose();
+        }
+    }
+}
