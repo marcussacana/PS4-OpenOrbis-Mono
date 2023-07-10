276b83d926b2ecd0fa0dd8abaf6c3113de85e171
Marcus André <ddtank.marcus@gmail.com>
2023-07-10 18:52:58 -0300

    Add Triangle2D Object

---
+++ main/OrbisGL/GL2D/Triangle2D.cs
@@ -0,0 +1,93 @@
+﻿using OrbisGL.GL;
+using SharpGLES;
+using SixLabors.ImageSharp;
+using System.Numerics;
+using static OrbisGL.GL2D.Coordinates2D;
+
+namespace OrbisGL.GL2D
+{
+    public class Triangle2D : GLObject2D
+    {
+        private readonly int RotateUniformLocation;
+        private readonly int BorderUniformLocation;
+        private readonly int ColorUniformLocation;
+
+        public float RoundLevel { get; set; } = 0.0f;
+        public RGBColor Color { get; set; } = RGBColor.White;
+        public byte Transparency { get; set; } = 255;
+
+        public Degrees Rotation { get; set; } = 0;
+
+        public enum Degrees : int {
+            Degree0 = 0,
+            Degree45 = 5,
+            Degree90 = 10,
+            Degree135 = 15,
+            Degree180 = 20,
+            Degree225 = 25,
+            Degree270 = 30,
+            Degree315 = 35
+        }
+
+        public Triangle2D(GL.Rectangle Rectangle) : this((int)Rectangle.Width, (int)Rectangle.Height)
+        {
+            Position = new Vector2(Rectangle.X, Rectangle.Y);
+        }
+        public Triangle2D(int Width, int Height)
+        {
+            this.Width = Width;
+            this.Height = Height;
+
+            var hProg = Shader.GetProgram(ResLoader.GetResource("VertexOffsetTexture"), ResLoader.GetResource("FragmentColorRoundedTriangle"));
+            Program = new GLProgram(hProg);
+
+            Program.AddBufferAttribute("Position", AttributeType.Float, AttributeSize.Vector3);
+            Program.AddBufferAttribute("uv", AttributeType.Float, AttributeSize.Vector2);
+
+            RenderMode = (int)OrbisGL.RenderMode.Triangle;
+
+            BorderUniformLocation = GLES20.GetUniformLocation(Program.Handler, "Border");
+            RotateUniformLocation = GLES20.GetUniformLocation(Program.Handler, "Rotate");
+            ColorUniformLocation = GLES20.GetUniformLocation(Program.Handler, "Color");
+
+            Program.SetUniform("Resolution", (float)Width, (float)Height);
+
+            RefreshVertex();
+        }
+        public override void RefreshVertex()
+        {
+            ClearBuffers();
+
+            //   0 ---------- 1
+            //   |            |
+            //   |            |
+            //   |            |
+            //   2 ---------- 3
+
+            AddArray(XToPoint(0), YToPoint(0), -1);//0
+            AddArray(0, 0);
+
+            AddArray(XToPoint(Width), YToPoint(0), -1);//1
+            AddArray(1, 0);
+
+            AddArray(XToPoint(0), YToPoint(Height), -1);//2
+            AddArray(0, 1);
+
+            AddArray(XToPoint(Width), YToPoint(Height), -1);//3
+            AddArray(1, 1);
+
+            AddIndex(0, 1, 2, 1, 2, 3);
+
+            base.RefreshVertex();
+        }
+
+        public override void Draw(long Tick)
+        {
+            Program.SetUniform(RotateUniformLocation, (float)Rotation / 10);
+            Program.SetUniform(BorderUniformLocation, RoundLevel);
+            Program.SetUniform(ColorUniformLocation, Color, Transparency);
+
+            base.Draw(Tick);
+        }
+    }
+}
---
+++ main/OrbisGL/Shaders/FragmentColorRoundedTriangle.glsl
@@ -0,0 +1,157 @@
+﻿#version 100
+
+precision highp float;
+
+varying vec2 UV;
+
+uniform vec4 Color;
+uniform vec2 Resolution;
+uniform float Border;
+uniform float Rotate;
+uniform vec4 VisibleRect;
+
+//Shadertoy Emulation
+#define fragColor gl_FragColor
+#define iResolution Resolution
+#define fragCoord Resolution * UV
+
+//stolen from https://www.shadertoy.com/view/tlVyWh
+
+float cro( in vec2 a, in vec2 b ) { return a.x*b.y - a.y*b.x; }
+
+
+vec3 sdgTriangle( in vec2 p, in vec2 v[3] )
+{
+    float gs = cro(v[0]-v[2],v[1]-v[0]);
+    vec4 res;
+    
+    // edge 0
+    {
+    vec2  e = v[1]-v[0];
+    vec2  w = p-v[0];
+    vec2  q = w-e*clamp(dot(w,e)/dot(e,e),0.0,1.0);
+    float d = dot(q,q);
+    float s = gs*cro(w,e);
+    res = vec4(d,q,s);
+    }
+    
+    // edge 1
+    {
+	vec2  e = v[2]-v[1];
+    vec2  w = p-v[1];
+    vec2  q = w-e*clamp(dot(w,e)/dot(e,e),0.0,1.0);
+    float d = dot(q,q);
+    float s = gs*cro(w,e);
+    res = vec4( (d<res.x) ? vec3(d,q) : res.xyz,
+                (s>res.w) ?      s    : res.w );
+    }
+    
+    // edge 2
+    {
+	vec2  e = v[0]-v[2];
+    vec2  w = p-v[2];
+    vec2  q = w-e*clamp(dot(w,e)/dot(e,e),0.0,1.0);
+    float d = dot(q,q);
+    float s = gs*cro(w,e);
+    res = vec4( (d<res.x) ? vec3(d,q) : res.xyz,
+                (s>res.w) ?      s    : res.w );
+    }
+    
+    // distance and sign
+    float d = sqrt(res.x)*sign(res.w);
+    
+    return vec3(d,res.yz/d);
+
+}
+
+#define AA 2
+
+void main()
+{
+    vec3 tot = vec3(0.0);
+    
+    float proportion;
+    
+    if (iResolution.y > iResolution.x)
+        proportion = (iResolution.x/iResolution.y) * iResolution.x;
+    else
+        proportion = (iResolution.y/iResolution.x) * iResolution.y;
+    
+    #if AA>1
+    for( int m=0; m<AA; m++ )
+    for( int n=0; n<AA; n++ )
+    {
+        // pixel coordinates
+        vec2 o = vec2(float(m),float(n)) / float(AA) - 0.5;
+        vec2 p = (-iResolution.xy + 2.0*(fragCoord+o))/proportion;
+        #else    
+        vec2 p = (-iResolution.xy + 2.0*fragCoord)/proportion;
+        #endif
+
+        // corner radious
+        float ra = 0.1;
+        
+        float rd = ra/10.;
+        
+        float MinX = -1. + rd;
+        float MaxX =  1. - rd;
+        float MidY = 0.;
+        float MidX = 0.;
+        float MinY = -1.;
+        float MaxY =  1.;
+        
+        vec2 v[3];        
+        
+        
+        if (Rotate >= 3.5) {
+            v[0] = vec2(MinX, MinY) * (1. - ra);
+            v[1] = vec2(MaxX, MaxY) * (1. - ra);
+            v[2] = vec2(MinX, MaxY) * (1. - ra);
+        } else if (Rotate >= 3.) {
+            v[0] = vec2(MaxX, MinY) * (1. - ra);
+            v[1] = vec2(MaxX, MaxY) * (1. - ra);
+            v[2] = vec2(MinX, MidY) * (1. - ra);
+        } else if (Rotate >= 2.5){
+            v[0] = vec2(MinX, MaxY) * (1. - ra);
+            v[1] = vec2(MaxX, MinY) * (1. - ra);
+            v[2] = vec2(MinX, MinY) * (1. - ra);
+        } else if (Rotate >= 2.){
+            v[0] = vec2(MinX, MaxY) * (1. - ra);
+            v[1] = vec2(MaxX, MaxY) * (1. - ra);
+            v[2] = vec2(MidX, MinY) * (1. - ra);
+        } else if (Rotate >= 1.5){
+            v[0] = vec2(MaxX, MinY) * (1. - ra);
+            v[1] = vec2(MinX, MinY) * (1. - ra);
+            v[2] = vec2(MaxX, MaxY) * (1. - ra);
+        } else if (Rotate >= 1.){
+            v[0] = vec2(MinX, MaxY) * (1. - ra);
+            v[1] = vec2(MinX, MinY) * (1. - ra);
+            v[2] = vec2(MaxX, MidY) * (1. - ra);
+        } else if (Rotate >= 0.5){
+            v[0] = vec2(MinX, MaxY) * (1. - ra);
+            v[1] = vec2(MaxX, MinY) * (1. - ra);
+            v[2] = vec2(MaxX, MaxY) * (1. - ra);
+        } else {
+            v[0] = vec2(MinX, MinY) * (1. - ra);
+            v[1] = vec2(MaxX, MinY) * (1. - ra);
+            v[2] = vec2(MidX, MaxY) * (1. - ra);
+        }
+
+        // sdf(p) and gradient(sdf(p))
+        vec3  dg = sdgTriangle(p,v);
+        float d = dg.x-ra;
+
+        // coloring
+        vec3 col = (d>0.0) ? vec3(0.) : vec3(1.);
+
+ 	    tot += col;
+    #if AA>1
+    }
+    tot /= float(AA*AA);
+    #endif
+
+	fragColor = vec4(Color.xyz * tot, tot * Color.w);
+    
+    if ((UV.x < VisibleRect.x || UV.y < VisibleRect.y || UV.x > VisibleRect.z + VisibleRect.x || UV.y > VisibleRect.w + VisibleRect.y) && VisibleRect != vec4(0))
+        discard;
+}
